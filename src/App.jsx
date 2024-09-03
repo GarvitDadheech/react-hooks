6627b767d6092637a631bc291d0f506d32bc6ec4
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-03 12:03:24 +0530

    Assignment3 usememo done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Assignment1 from "./components/useMemo/Assignment1"
 import Assignment2 from "./components/useMemo/Assignment2"
+import Assignment3 from "./components/useMemo/Assignment3"
 
 function App() {
 
   return (
     <>
     {/* <Assignment1/> */}
-    <Assignment2/>
+    {/* <Assignment2/> */}
+    <Assignment3/>
     </>
   )
 }
--- src/components/useMemo/Assignment3.jsx
+++ src/components/useMemo/Assignment3.jsx
@@ -1,2 +1,48 @@
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
+
+import { useState,useMemo } from "react";
+
+const groceryItems = [
+    {
+        name: "Milk",
+        quantity: 2,
+        pricePerUnit: 2
+    },
+    {
+        name: "Bread",
+        quantity: 1,
+        pricePerUnit: 2
+    },
+    {
+        name: "Eggs",
+        quantity: 12,
+        pricePerUnit: 4
+    },
+    {
+        name: "Apples",
+        quantity: 6,
+        pricePerUnit: 2
+    }
+];
+
+function Assignment3() {
+
+    const [groceryItemList,setGroceryItemList] = useState(groceryItems);
+
+    const totalAmount = useMemo(() => {
+        let value = 0;
+        for(const item of groceryItemList) {
+            console.log(item);
+            value += item.quantity*item.pricePerUnit;
+        }
+        return value;
+    },[groceryItemList]);
+
+    return (
+        <div>
+            <h1>The total amount spent is {totalAmount}</h1>
+        </div>
+    )
+}
+export default Assignment3;
